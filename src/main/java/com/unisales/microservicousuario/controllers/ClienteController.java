a36f0d6cef7b0b0008e54430560258f7d13068de
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-11-28 16:58:21 -0300

    aa

--- src/main/java/com/unisales/microservicousuario/controllers/ClienteController.java
+++ src/main/java/com/unisales/microservicousuario/controllers/ClienteController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import com.unisales.microservicousuario.services.ClienteService;
 
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
